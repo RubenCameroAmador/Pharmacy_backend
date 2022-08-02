dff2ea4073fde97f53af51729ffaad2fb18c49d6
RubenCameroAmador <37782994+RubenCameroAmador@users.noreply.github.com>
2022-08-01 21:37:12 -0500

    Desarrollo de los serializer de product and bill

--- farmaciaApp/serializers/billSerializer.py
+++ farmaciaApp/serializers/billSerializer.py
@@ -1,7 +1,32 @@
 from farmaciaApp.models.bill import Bill
+from farmaciaApp.models.ordenDetail import OrdenDetail
+from farmaciaApp.serializers.ordenDetailSerializer import OrdenDetailSerializer
 from rest_framework import serializers
 
 class BillSerializer( serializers.ModelSerializer):
+    ordenDetail = OrdenDetailSerializer()
     class Meta:
         model = Bill
-        fields = ['transaction_date', 'total_price','state_orden']
+        fields = ['transaction_date', 'total_price','state_orden','ordenDetail'] #agregué el campo ordenDetail
+
+    def to_representation(self, obj):
+        bill = Bill.objects.get(id= obj.id)
+        ordenDetail = OrdenDetail.objects.get(bill = obj.id) 
+        return {
+            'id':bill.id,
+            'transaction_date': bill.transaction_date,
+            'total_price':bill.total_price,
+            'state_orden': bill.state_orden,
+            'ordenDetail': {
+                'id': ordenDetail.id,
+                'product': ordenDetail.product,     #esto no lo tengo claro. El product es otra FK
+                'price': ordenDetail.price,
+                'amount': ordenDetail.amount,
+            }
+        }
+
+    def create(self, validated_data):
+        ordenDetailData = validated_data.pop('ordenDetail')
+        billInstance = Bill.objects.create(**validated_data)
+        OrdenDetail.objects.create(bill = billInstance,**ordenDetailData)
+        return billInstance
--- farmaciaApp/serializers/productSerializer.py
+++ farmaciaApp/serializers/productSerializer.py
@@ -1,8 +1,39 @@
-from dataclasses import fields
+from itertools import product
 from farmaciaApp.models.product import Product
+from farmaciaApp.models.ordenDetail import OrdenDetail
+from farmaciaApp.serializers.ordenDetailSerializer import OrdenDetailSerializer
 from rest_framework import serializers
 
 class ProductSerializer(serializers.ModelSerializer):
+    ordenDetail = OrdenDetailSerializer()
     class Meta:
         model= Product,
-        fields= ['name','description','unit_price','expiration_date','amount','cooled','image_link','category']
+        fields= ['name','description','unit_price','expiration_date','amount','cooled','image_link','category','ordenDetail']  #Agregué ordenDetail
+
+    def to_representation(self, obj):
+        product = Product.objects.get(id= obj.id)
+        ordenDetail = OrdenDetail.objects.get(product = obj.id)
+        return {
+            'id':product.id,
+            'name': product.name,
+            'description':product.description,
+            'unit_price': product.unit_price,
+            'expiration_date': product.expiration_date,
+            'amount': product.amount,
+            'cooled': product.cooled,
+            'image_link': product.image_link,
+            'category': product.category,
+            'ordenDetail': {
+                'id': ordenDetail.id,
+                'product': ordenDetail.product,     #esto no lo tengo claro. El product es otra FK
+                'price': ordenDetail.price,
+                'amount': ordenDetail.amount,
+            }
+        }
+    
+    def create(self, validated_data):
+        ordenDetailData = validated_data.pop('ordenDetail')
+        productInstance = Product.objects.create(**validated_data)
+        OrdenDetail.objects.create(product = productInstance,**ordenDetailData)
+        return productInstance
+
